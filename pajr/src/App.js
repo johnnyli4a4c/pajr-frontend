0fdf64c3b2b30e26320567f4449da7871e3d79b7
Johnny Li <c_johnny@hotmail.com>
2017-10-14 23:10:29 +0000

    Added layout for items

--- pajr/src/App.js
+++ pajr/src/App.js
@@ -2,23 +2,68 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Grid = require('react-bootstrap').Grid;
+const Row = require('react-bootstrap').Row;
+const Col = require('react-bootstrap').Col;
+const ListGroup = require('react-bootstrap').ListGroup;
+const ListGroupItem = require('react-bootstrap').ListGroupItem;
+const ControlLabel = require('react-bootstrap').ControlLabel;
+const FormGroup = require('react-bootstrap').FormGroup;
+const FormControl = require('react-bootstrap').FormControl;
+
+var items = [
+    {name: "Johnny", message: "Help", disaster: "fire", status: "queue", priority: 1},
+    {name: "Anthony", message: "Message 1", disaster: "Disaster 2", status: "queue", priority: 3},
+    {name: "Ben", message: "Message 2", disaster: "earthquake", status: "queue", priority: 2}
+  ]
+
+// Logic
+function listsGroupInstance(items) {
+  var listGroupRows = []
+  items.forEach(function(element) {
+    listGroupRows.push(<ListGroupItem>{element.name}</ListGroupItem>)
+  }, this);
+  return <ListGroup>{listGroupRows}</ListGroup>
+}
+
+function itemDetails(item) {
+  return (
+    <form>
+      <FormGroup>
+        <ControlLabel>Name</ControlLabel>
+        <FormControl value={item.name} />
+      </FormGroup>
+      <FormGroup>
+        <ControlLabel>Message</ControlLabel>
+        <FormControl value={item.message} />
+      </FormGroup>
+      <FormGroup>
+        <ControlLabel>priority</ControlLabel>
+        <FormControl value={item.priority} />
+      </FormGroup>
+    </form>
+  )
+}
+
+
+// Template
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Grid>
+          <Row>
+            <Col md={3} style={{"background-color": "red"}}>{listsGroupInstance(items)}</Col>
+            <Col md={9}>{itemDetails(items[0])}</Col>
+          </Row>
+        </Grid>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
-
-
 
 
 export default App;
